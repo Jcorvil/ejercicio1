1cd5dcabd0de96133d737ba567e5dffd67f4e6a2
Jorge <jcorvil600@g.educaand.es>
2022-10-21 12:34:35 +0200

    Update ejercicio1.java
    He añadido "Adiós mundo"

--- ejercicio1.java
+++ ejercicio1.java
@@ -2,5 +2,6 @@ public class ejercicio1 {
     public static void main(String[] args) {
         String saludo = "Hola";
         System.out.printf("%s", saludo);
+        System.out.printf("Adios mundo");
     }
 }
